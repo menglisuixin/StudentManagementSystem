100381d3d258ae62aa9c77296d3969a58412fe36
梦里 <3193921314@qq.com>
2025-03-07 17:57:18 +0800

    表单校验

--- vue3StudentProject/src/view/login/index.vue
+++ vue3StudentProject/src/view/login/index.vue
@@ -6,16 +6,17 @@
       status-icon
       label-width="80px"
       class="login-form"
+      :rules="rules"
     >
       <h2 class="login-title">登&emsp;录</h2>
-      <el-form-item label="账号">
+      <el-form-item label="账号" prop="username">
         <el-input v-model="form.username" type="text" placeholder="请输入账号" />
       </el-form-item>
-      <el-form-item label="密码">
+      <el-form-item label="密码" prop="password">
         <el-input v-model="form.password" type="password" placeholder="请输入密码" />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit()"> 登录 </el-button>
+        <el-button type="primary" @click="onSubmit(ruleFormRef)"> 登录 </el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -23,7 +24,8 @@
 
 <script lang="ts" setup>
 import { reactive, ref } from "vue";
-import type { FormInstance } from "element-plus";
+import type { FormInstance, FormRules } from "element-plus";
+
 const ruleFormRef = ref<FormInstance>();
 
 const form = reactive({
@@ -31,8 +33,49 @@ const form = reactive({
   password: "",
 });
 
-let onSubmit = () => {
-  console.log("登录");
+let validateUserName = (_rule: any, value: any, callback: any) => {
+  value = value.trim();
+  const length = value && value.length;
+  const pwdReg = /^[A-z0-9_]+$/;
+  if (value == "") {
+    callback(new Error("请输入账号"));
+  } else if (length < 4 || length > 12) {
+    callback(new Error("长度必须在4-12之间"));
+  } else if (!pwdReg.test(value)) {
+    callback(new Error("必须是数字字母下划线组成"));
+  } else {
+    callback();
+  }
+};
+
+let rules = reactive<FormRules>({
+  username: [
+    {
+      required: true,
+      validator: validateUserName,
+      trigger: ["change", "blur"],
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "请输入密码",
+      trigger: "blur",
+    },
+  ],
+});
+
+let onSubmit = (formEl: FormInstance | undefined) => {
+  if (!formEl) {
+    return;
+  }
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log("success");
+    } else {
+      console.log("error");
+    }
+  });
 };
 </script>
 <style scoped>
